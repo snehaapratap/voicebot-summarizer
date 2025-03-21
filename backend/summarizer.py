0529d54c570471c118a37f778b97e79e6a899a86
Sneha P Pratap <snehapratap248@gmail.com>
2025-03-21 16:39:39 +0530

    done

--- backend/summarizer.py
+++ backend/summarizer.py
@@ -1,6 +1,7 @@
 from transformers import pipeline
 
-summarizer = pipeline("summarization")
+
+summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
 
 def summarize_text(text):
     summary = summarizer(text, max_length=50, min_length=20, do_sample=False)
